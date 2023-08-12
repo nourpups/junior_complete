84e20c8994802a42b14aa6bdd2b76bd94e1d3fbd
nourpups <nourgithub@gmail.com>
2023-08-12 12:23:26 +0500

    adding message 'privetstviya'

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,143 +1,37 @@
 @extends('layouts.backend')
 
-@section('js')
+{{--@section('js')--}}
 
-  <script src="{{asset('js/plugins/easy-pie-chart/jquery.easypiechart.min.js')}}"></script>
-  <script>Codebase.helpersOnLoad(['jq-easy-pie-chart']);</script>
+{{--  <script src="{{asset('js/plugins/easy-pie-chart/jquery.easypiechart.min.js')}}"></script>--}}
+{{--  <script>Codebase.helpersOnLoad(['jq-easy-pie-chart']);</script>--}}
 
-@endsection
+{{--@endsection--}}
 
 @section('content')
   <!-- Page Content -->
-  <div class="content">
-    <div class="row items-push">
-      <div class="col-md-6 col-xl-3">
-        <div class="block block-rounded">
-          <div class="block-content block-content-full bg-warning">
-            <div class="js-pie-chart pie-chart" data-percent="30" data-line-width="4" data-size="120" data-bar-color="#ffffff" data-track-color="rgba(255,255,255,.25)" data-scale-color="rgba(255,255,255,.5)">
-              <span class="text-white">6 Slots</span>
-            </div>
-          </div>
-          <div class="block-content block-content-full text-center">
-            <a class="btn rounded-pill btn-alt-warning" href="javascript:void(0)">
-              <i class="fa fa-plus opacity-50 me-1"></i> Add new
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-xl-4">
-        <div class="block block-rounded h-100 mb-0">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">
-              Block Title
-            </h3>
-          </div>
-          <div class="block-content font-size-sm text-muted">
-            <p>
-              ...
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-xl-4">
-        <div class="block block-rounded h-100 mb-0">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">
-              Block Title
-            </h3>
-          </div>
-          <div class="block-content font-size-sm text-muted">
-            <p>
-              ...
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-xl-4">
-        <div class="block block-rounded h-100 mb-0">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">
-              Block Title
-            </h3>
-          </div>
-          <div class="block-content font-size-sm text-muted">
-            <p>
-              ...
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-xl-4">
-        <div class="block block-rounded h-100 mb-0">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">
-              Block Title
-            </h3>
-          </div>
-          <div class="block-content font-size-sm text-muted">
-            <p>
-              ...
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-xl-4">
-        <div class="block block-rounded h-100 mb-0">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">
-              Block Title
-            </h3>
-          </div>
-          <div class="block-content font-size-sm text-muted">
-            <p>
-              ...
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-xl-4">
-        <div class="block block-rounded h-100 mb-0">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">
-              Block Title
-            </h3>
-          </div>
-          <div class="block-content font-size-sm text-muted">
-            <p>
-              ...
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-xl-4">
-        <div class="block block-rounded h-100 mb-0">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">
-              Block Title
-            </h3>
-          </div>
-          <div class="block-content font-size-sm text-muted">
-            <p>
-              ...
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-xl-4">
-        <div class="block block-rounded h-100 mb-0">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">
-              Block Title
-            </h3>
-          </div>
-          <div class="block-content font-size-sm text-muted">
-            <p>
-              ...
-            </p>
-          </div>
-        </div>
+
+<div class="bg-primary">
+    <div class="content text-center">
+      <div class="py-5">
+        <h1 class="fw-bold text-white mb-2">Kakoy-to ViDeLaYuSHiYsYa tekst dlya privetstviya</h1>
+        <h2 class="h4 fw-normal text-white-75">kakaya-ta riba dlya zapolneniya etogo h2 </h2>
+         <small class="text-primary-light">v budushem obyazatelno zapolnu etu stranitsu</small>
       </div>
     </div>
-  </div>
+</div>
+{{--      <div class="col-md-6 col-xl-3">--}}
+{{--        <div class="block block-rounded">--}}
+{{--          <div class="block-content block-content-full bg-warning">--}}
+{{--            <div class="js-pie-chart pie-chart" data-percent="30" data-line-width="4" data-size="120" data-bar-color="#ffffff" data-track-color="rgba(255,255,255,.25)" data-scale-color="rgba(255,255,255,.5)">--}}
+{{--              <span class="text-white">6 Slots</span>--}}
+{{--            </div>--}}
+{{--          </div>--}}
+{{--          <div class="block-content block-content-full text-center">--}}
+{{--            <a class="btn rounded-pill btn-alt-warning" href="javascript:void(0)">--}}
+{{--              <i class="fa fa-plus opacity-50 me-1"></i> Add new--}}
+{{--            </a>--}}
+{{--          </div>--}}
+{{--        </div>--}}
+{{--      </div>--}}
   <!-- END Page Content -->
 @endsection
